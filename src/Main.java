b383e8055b05cd3a46ed668559c20cc9003461ea
Иван <ivan.shutov95@mail.ru>
2025-07-26 22:12:41 +0300

    Hw 1

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Задача 1");
-        int firstFriday = 1;
-        for (int i = 1; i < 31; i = i + 7 ){
-            System.out.println(" Сегодня пятница " + i + " -е число " + (firstFriday + i));
+        int firstFriday = 4;
+        for (int i = 1; i < 31; i++) {
+            if ((i - firstFriday) % 7 == 0)
+                System.out.println(" Сегодня пятница " + i + " -е число ");
         }
+        System.out.println("Задача 2");
 
-}
+    }
 }
