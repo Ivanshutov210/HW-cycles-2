017dea76b4ddef9fc90515e7a32e30b12237f93b
Иван <ivan.shutov95@mail.ru>
2025-07-26 19:50:06 +0300

    Hw 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Задача 1");
+        int firstFriday = 2;
+        for (int i = 1; i < 31; i = i + 7 ){
+            System.out.println(" Сегодня пятница " + i + " -е число " + (firstFriday + i));
+        }
+
+}
+}
